5c8c03fcd4054bb85d07212b954ac035130e297f
mobin <mobinmohammadi4321@gmail.com>
2025-05-29 20:46:01 +0330

    addRegisterForm

---
+++ src/assets/Components/RegistrationAndAuthentication/RegistrationAndAuthentication.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function RegistrationAndAuthentication() {
+  return (
+    <div>RegistrationAndAuthentication</div>
+  )
+}
---
+++ src/main.jsx
@@ -0,0 +1,27 @@
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { StrictMode } from "react";
+import Home from "./assets/Page/Home/Home.jsx";
+import CateguryFoods from "./assets/Components/CateguryFoods/CateguryFoods.jsx";
+import SinglePageRestorant from "./assets/Components/SinglePageRestorant/SinglePageRestorant.jsx";
+import { Pane } from "react-leaflet";
+import PanelSellerResturants from "./assets/Page/Home/PanelSellerResturants/PanelSellerResturants.jsx";
+import NewRestourants from "./assets/Components/PanelSaller/HomeSellerPanel/NewRestourants/NewRestourants.jsx";
+import HomePanel from "./assets/Components/PanelSaller/HomeSellerPanel/HomePanel/HomePanel.jsx";
+import SignUp from "./assets/Page/Home/SignUp/SignUp.jsx";
+createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/categuryfoods" element={<CateguryFoods />} />
+      <Route path="/restorant/:ResoruntID" element={<SinglePageRestorant />} />
+      <Route path="/restorant/Signup" element={<SignUp />} />
+      <Route path="/sallerpanel/*" element={<PanelSellerResturants />}>
+        <Route path="home" element={<HomePanel />} />
+        <Route path="newrestorants" element={<NewRestourants />} />
+      </Route>
+    </Routes>
+  </BrowserRouter>
+);
